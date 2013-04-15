f018d1cef054ea2cceec8e33cf5628ed6ce9bb01
HondaDai <hondadai.tw@gmail.com>
2013-04-15 20:25:20 +0800

    add scan 'available network' & upgrade to 2.6.0

--- Readme.md
+++ Readme.md
@@ -3,7 +3,7 @@ PhoneGap WifiInfoPlugin
 
 * Author: HondaDai
 * License - The MIT License
-* Test on cordova 2.2.0 (PhoneGap 2.2.0)
+* Test on cordova 2.6.0 (PhoneGap 2.6.0)
 
 
 Install Step (On Eclipse)
@@ -19,20 +19,46 @@ API
 
 ### Example 1
 	window.plugins.WifiInfo.get(function(wifi){ console.log(wifi); });
-	> undefined
-	> Object
-		   BSSID: "00:21:91:08:63:19"
-		   HiddenSSID: false
-		   IpAddress: -1191139136
-		   LinkSpeed: 65
-		   MacAddress: "E0:B9:A5:8B:6E:95"
-		   NetworkId: 0
-		   RSSI: -55
-		   SSID: "dlink"
-		   __proto__: Object
 
 ### Example 2
 	window.plugins.WifiInfo.get(function(wifi){ 
 		alert(wifi.SSID); 
 	});
+	
+### Output Format
+    object(6): {
+       activity:  object(8): {
+          MacAddress:  string(17): "4c:aa:16:ad:c8:xx"
+          NetworkId:  number: 8
+          SSID:  string(6): "HYHome"
+          LinkSpeed:  number: 135
+          HiddenSSID:  boolean: false
+          RSSI:  number: -46
+          IpAddress:  number: 67217600
+          BSSID:  string(17): "78:44:76:de:f1:xx"
+       }
+       available:  array(2): {
+          [0]:  object(5): {
+             capabilities:  string(24): "[WPA-PSK-CCMP][WPS][ESS]"
+             level:  number: -49
+             frequency:  number: 2462
+             BSSID:  string(17): "78:44:76:de:f1:xx"
+             SSID:  string(6): "HYHome"
+          }
+          [1]:  object(5): {
+             capabilities:  string(20): "[WPA2-PSK-CCMP][ESS]"
+             level:  number: -62
+             frequency:  number: 2412
+             BSSID:  string(17): "fc:f5:28:5e:f4:xx"
+             SSID:  string(7): "CHT9489"
+          }
+       }
+    } at file:///android_asset/www/js/index.js:40
 
+
+Changelog
+-----
+
+* 2013/4/5 
+  * adjust output format
+  * add **available network**
--- WifiInfoPlugin.java
+++ WifiInfoPlugin.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
@@ -21,16 +22,33 @@ public class WifiInfoPlugin extends CordovaPlugin {
 		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 		
+		
 		JSONObject obj = new JSONObject();
 		try {
-			obj.put("BSSID", wifiInfo.getBSSID());
-			obj.put("HiddenSSID", wifiInfo.getHiddenSSID());
-			obj.put("SSID", wifiInfo.getSSID());
-			obj.put("MacAddress", wifiInfo.getMacAddress());
-			obj.put("IpAddress", wifiInfo.getIpAddress());
-			obj.put("NetworkId", wifiInfo.getNetworkId());
-			obj.put("RSSI", wifiInfo.getRssi());
-			obj.put("LinkSpeed", wifiInfo.getLinkSpeed());
+			JSONObject activity = new JSONObject();
+			activity.put("BSSID", wifiInfo.getBSSID());
+			activity.put("HiddenSSID", wifiInfo.getHiddenSSID());
+			activity.put("SSID", wifiInfo.getSSID());
+			activity.put("MacAddress", wifiInfo.getMacAddress());
+			activity.put("IpAddress", wifiInfo.getIpAddress());
+			activity.put("NetworkId", wifiInfo.getNetworkId());
+			activity.put("RSSI", wifiInfo.getRssi());
+			activity.put("LinkSpeed", wifiInfo.getLinkSpeed());
+			obj.put("activity", activity); 
+			
+			JSONArray available = new JSONArray();
+	        for (ScanResult scanResult : wifiManager.getScanResults()) {
+	        	JSONObject ap = new JSONObject();
+	        	ap.put("BSSID", scanResult.BSSID);
+	        	ap.put("SSID", scanResult.SSID);
+	        	ap.put("frequency", scanResult.frequency);
+	        	ap.put("level", scanResult.level);
+	        	//netwrok.put("timestamp", String.valueOf(scanResult.timestamp));
+	        	ap.put("capabilities", scanResult.capabilities);
+	        	available.put(ap);
+	        }
+	        obj.put("available", available); 
+	        
 			
 		} catch (JSONException e) {
 			e.printStackTrace();
