3a8f460f9b75aec20a7d426e8cd242fc0df1f4ec
Blake Byrnes <blakebyrnes@gmail.com>
2014-04-29 13:50:40 -0400

    Add callback to wifi info plugin

--- www/cordovaWifiInfo.js
+++ www/cordovaWifiInfo.js
@@ -5,15 +5,16 @@ var exec = require('cordova/exec'),
 function WifiInfo() {
   this.lan = {};
   this.networks = {};
-  this.refresh = function () {
-	$me = this;
+  this.refresh = function (cb) {
     this._check(function (data) {
-    	$me.lan = data.lan;
-    	$me.networks = data.networks;
-    }, function (e) {
+  	console.log("got refreshed data " + JSON.stringify(data.lan));
+    	this.lan = data.lan;
+    	this.networks = data.networks;
+        if (cb) cb();
+    }.bind(this), function (e) {
       throw ("Error initializing Wifi Information: " + e);
     });
-  };
+  }.bind(this);
 }
 
 WifiInfo.prototype._check = function (success, fail) {
@@ -23,19 +24,7 @@ WifiInfo.prototype._check = function (success, fail) {
 
 var me = new WifiInfo();
 
-channel.onCordovaReady.subscribe(function () {
-  me._check(function (info) {
-  	me.lan = info.lan;
-	me.networks = info.networks;
-    },
-    function (e) {
-      throw ("Error initializing Network Wifi Information: " + e);
-    });
-    
-    //listen for events on network
-    document.addEventListener("online", me.refresh, false);
-    document.addEventListener("offline", me.refresh, false);
-});
+channel.onCordovaReady.subscribe(me.refresh);
 
 
 module.exports = me;
